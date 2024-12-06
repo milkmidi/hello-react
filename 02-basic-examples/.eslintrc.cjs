f70f6d5f58779abaf49874afbd7793d47fd28b37
milkmidi <milkmidi.liao@positivegrid.com>
2024-12-06 08:15:38 +0800

    feat: upgrade react version

--- 02-basic-examples/.eslintrc.cjs
+++ 02-basic-examples/.eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['perfectionist', 'react-refresh'],
   settings: {
     'import/resolver': {
       node: {
@@ -28,6 +28,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     'import/no-import-module-exports': 0,
     'import/prefer-default-export': 0,
+    'import/order': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/label-has-associated-control': 0,
@@ -46,5 +47,50 @@ module.exports = {
     'react/prop-types': 0,
     'react/react-in-jsx-scope': 0,
     'react/require-default-props': 0,
+    'perfectionist/sort-jsx-props': [
+      'error',
+      {
+        type: 'natural',
+        groups: [
+          'dataComponent',
+          'keyRef',
+          'id',
+          'className',
+          'shorthand',
+          'data',
+          'unknown',
+          'multiline',
+          'callback',
+        ],
+        customGroups: {
+          dataComponent: 'data-component',
+          keyRef: ['key', 'ref'],
+          id: 'id',
+          className: 'className',
+          data: 'data-*',
+          callback: 'on*',
+        },
+      },
+    ],
+    // https://eslint-plugin-perfectionist.azat.io/rules/sort-imports
+    'perfectionist/sort-imports': [
+      'error',
+      {
+        newlinesBetween: 'never',
+        internalPattern: ['^@/.*'],
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          'internal-type',
+          ['parent-type', 'sibling-type', 'index-type'],
+          ['parent', 'sibling', 'index'],
+          'object',
+          'unknown',
+          'style',
+        ],
+      },
+    ],
+    // https://eslint-plugin-perfectionist.azat.io/rules/sort-named-imports
+    'perfectionist/sort-named-imports': ['error'],
   },
 };
